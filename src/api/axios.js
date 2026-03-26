4131357eed7853cba9af08c65d04fe991297daad
skywalker-4567 <utkarshksingh.34567@gmail.com>
2026-03-27 00:06:11 +0530

    update---vite api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
-})
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080',
+});
 
 // Before every request, automatically attach the JWT token if it exists
 api.interceptors.request.use((config) => {
